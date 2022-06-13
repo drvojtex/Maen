c8da06478ddbe5d979ffc37f340aebf45a74a1b3
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-06-13 22:34:24 +0200

    eco up

--- src/EcosystemUtils.jl
+++ src/EcosystemUtils.jl
@@ -2,7 +2,7 @@
 using Graphs
 using BSON, JSON
 
-function create_ecosystem(setup_file::String)
+function create_ecosystem(setup_file::String, model_params::AbstractArray)
 
     # Load the setup file
     setup_json::Dict{String, Any} = JSON.parsefile(setup_file)
@@ -23,7 +23,6 @@ function create_ecosystem(setup_file::String)
         write(f, "\n$m")
     end
 
-    global model_params
     bson("$(dirname(setup_file))/ecosystem.bson", graph=graph, components=components, ps=model_params)
 
 end
