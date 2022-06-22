4e608f54d1e8902b37808c6f469b8605aa09f4a1
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-06-23 00:06:22 +0200

    example up

--- examples/stock/learn_ecosystem.jl
+++ examples/stock/learn_ecosystem.jl
@@ -1,8 +1,7 @@
 
 using Maen
-using StatsBase, Statistics, IterTools, Graphs
-using BSON, JSON
-using Flux, Zygote, MLDataPattern
+using StatsBase, Statistics, IterTools
+using Flux, MLDataPattern
 using Flux: @epochs
 
 #map(x->x[1].=x[2], zip(model_params, ecosystem[:model_params]))
@@ -13,14 +12,13 @@ minibatchrun = (x) -> map(data->model(eco, data), eachcol(x))
 
 acc = (x, y) -> mean((reduce(vcat, minibatchrun(x)) .> .5) .== Int.(reduce(vcat, y)))
 
-loss = (x, y) -> Flux.mae(reduce(vcat, minibatchrun(x)), reduce(vcat, y))
+loss = (x, y) -> Flux.huber_loss(reduce(vcat, minibatchrun(x)), reduce(vcat, y); δ=0.4)
 
 cb = () -> println("accuracy = ", acc(data, labels), 
     " loss = ", loss(data, labels))
 
 @epochs 3 Flux.Optimise.train!(
         loss, Flux.params(params_objects), repeatedly(minibatch, 1000), 
-        ADAM(.0001), cb = Flux.throttle(cb, 5))
+        ADAM(), cb = Flux.throttle(cb, 5))
 
 println("total: accuracy = ", acc(data, labels), " loss = ", loss(data, labels))
-
--- examples/stock/prepare_data.jl
+++ examples/stock/prepare_data.jl
@@ -5,7 +5,7 @@ using StatsBase
 println("Creating dataset...")
 
 window = 50
-df = filter(x->x.Name=="AAPL", CSV.read("all_stocks_5yr.csv", DataFrame))
+df = filter(x->x.Name=="JPM", CSV.read("all_stocks_5yr.csv", DataFrame))
 
 data = []
 labels = []
@@ -13,10 +13,10 @@ labels = []
 for i=1:size(df)[1]-100
     tmp = deepcopy(df[i:i+window-1, 2:5])  # get 100 days of stock
     for i=1:4
-        tmp[:, i] .- mean(tmp[:, i])
+        tmp[:, i] .-= mean(tmp[:, i])
     end
     next7 = mean(deepcopy(df[i+window-1+10:i+window-1+20, 5])) # 10-20 days later mean
-    next7 .- mean(tmp[:, 4])
+    next7 -= mean(deepcopy(df[i:i+window-1, 5]))
     
     append!(data, [map(x-> Vector{Float32}(x), [tmp[:, 1], tmp[:, 2], tmp[:, 3], tmp[:, 4]])])
     append!(labels, [Matrix{Float32}([Int(tmp[:, 4][end] < next7)]')])
@@ -25,6 +25,6 @@ end
 data = permutedims(mapreduce(permutedims, vcat, data))
 
 function minibatch()
-    idx = sample(1:length(labels), 10, replace=true)
+    idx = sample(1:length(labels), 50, replace=true)
     data[:, idx], labels[idx]
 end
