5ee8104bb387f6fc6b5b0334eac2ed5c20a51698
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-06-29 00:42:30 +0200

    added shapley

--- src/Maen.jl
+++ src/Maen.jl
@@ -1,9 +1,9 @@
 module Maen
 
-export shapley
 export create_ecosystem, model, scv, scheduling, simple_visu
 export Component, Ecosystem
 export InputAgent, HiddenAgent, NetworkAgent
+export shapley
 
 abstract type AbstractComponent{T} end
 abstract type ComponentType end
@@ -11,9 +11,9 @@ abstract type InputAgent <: ComponentType end
 abstract type HiddenAgent <: ComponentType end
 abstract type NetworkAgent <: ComponentType end
 
-include("Shapley.jl")
 include("EcosystemStructs.jl")
 include("EcosystemUtils.jl")
 include("Visualise.jl")
+include("Shapley.jl")
 
 end # module
--- src/Shapley.jl
+++ src/Shapley.jl
@@ -1,5 +1,4 @@
 
-using Maen
 using Combinatorics
 
 @doc """
