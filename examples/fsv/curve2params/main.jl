09f6ced1e5bc5dfd6d27108e5e1877e077151f13
drvojtex <drvojtex.github@gmail.com>
2023-03-14 11:02:55 +0100

    up

--- examples/fsv/curve2params/main.jl
+++
@@ -1,9 +0,0 @@
-using CDalgs, Maen
-using BSON, DataFrames
-using Graphs, SimpleWeightedGraphs
-using Flux, StatsBase, LinearAlgebra, IterTools, Random
-using CategoricalArrays
-
-include("prepare_data.jl")
-include("create_eco.jl")
-include("learn_eco.jl")
